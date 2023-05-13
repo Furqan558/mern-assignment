8a2d06bfbe082e12b87a01257c64506fa83688db
Furqan558 <furqanhameed558@gmail.com>
2023-05-13 15:16:08 +0500

    problem 1 included

--- src/codingtask/Component.js
+++ src/codingtask/Component.js
@@ -42,28 +42,42 @@ const Component = ({ data }) => {
 	return (
 		<div>
 			<h2>Problem 1</h2>
-			<pre>
-				Find the Problem: Use tools like Google PageSpeed to see which parts of
-				your website are slow. Check Your Server: If your server is slow to
-				respond, your website will be slow too. You can check things like server
-				logs and CPU usage to find any issues. Optimize Database: If your
-				website uses a database, make sure it's running efficiently. Slow
-				database queries can make your website slow. Check Network: Sometimes,
-				it's not your website but the network that's slow. Try your website on
-				different networks to see if it's still slow. Optimize Images and Files:
-				Large images and files can make your website slow. Make them smaller
-				without losing quality to help speed up your site. Use Caching: Caching
-				can help your website load faster for people who visit your website more
-				than once. Code Splitting: In React.js, you can divide your code into
-				smaller parts which can then be loaded as needed. This can help to
-				improve the speed of your website. Eliminate Blocking Resources: Some
-				resources, like JavaScript and CSS, can slow down your website if
-				they're loaded at the wrong time. Make sure these load at the right time
-				to avoid slowing down your site. Use of Service Workers: These can help
-				your site load faster on repeat visits, even when offline. Check Again:
-				After making changes, check your website's speed again to see if it's
-				improved.
-			</pre>
+			<ol>
+				<li>
+					Find the Problem: Use tools like Google PageSpeed to see which parts
+					of your website are slow.
+				</li>
+				Check Your Server: If your server is slow to respond, your website will
+				be slow too. You can check things like server logs and CPU usage to find
+				any issues.
+				<li>
+					Optimize Database: If your website uses a database, make sure it's
+					running efficiently. Slow database queries can make your website slow.
+				</li>
+				Check Network: Sometimes, it's not your website but the network that's
+				slow. Try your website on different networks to see if it's still slow.
+				<li>
+					Optimize Images and Files: Large images and files can make your
+					website slow. Make them smaller without losing quality to help speed
+					up your site.
+				</li>
+				Use Caching: Caching can help your website load faster for people who
+				visit your website more than once.
+				<li>
+					Code Splitting: In React.js, you can divide your code into smaller
+					parts which can then be loaded as needed. This can help to improve the
+					speed of your website.
+				</li>
+				Eliminate Blocking Resources: Some resources, like JavaScript and CSS,
+				can slow down your website if they're loaded at the wrong time. Make
+				sure these load at the right time to avoid slowing down your site.
+				<li>
+					Use of Service Workers: These can help your site load faster on repeat
+					visits, even when offline.
+				</li>
+				Check Again: After making changes, check your website's speed again to
+				see if it's improved.
+			</ol>
 			<h2>Component</h2>
 			<Table striped bordered hover>
 				<thead>
